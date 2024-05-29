7f72bf3af28aa15375e1ae07b3f0c469a6b220d2
Nicholas Oyagh <you@example.com>
2024-05-29 14:26:00 -0700

    slices updated

--- src/slices/taskSlice.ts
+++ src/slices/taskSlice.ts
@@ -1,35 +1,33 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import  axios  from "axios";
+import axios from "axios";
 
-type  taskType = {
-  tasks: string [],
+type taskType = {
+  tasks: string[],
 }
 
-const initialState: taskType = { 
+const initialState: taskType = {
   tasks: [],
 }
 
 const baseUrl = "https://project-4-server.netlify.app"
 
-export const getTasks = createAsyncThunk("tasks/getTasks", async (thunkAPI) => {
+export const getTasks = createAsyncThunk("tasks/getTasks", async (_, thunkAPI) => {
   try {
     const res = await axios.get(`${baseUrl}/.netlify/functions/api`);
     return res.data;
   } catch (err) {
-    console.log(err);
+    return thunkAPI.rejectWithValue(err);
   }
 });
 
 export const deleteTask = createAsyncThunk(
   "tasks/deleteTask",
-  async (data:{id:number}, thunkAPI) => {
-    
-    const id = data.id
+  async (data: { id: number }, thunkAPI) => {
     try {
-      await axios.delete(`${baseUrl}/.netlify/functions/api/${id}`);
-      return id;
+      await axios.delete(`${baseUrl}/.netlify/functions/api/${data.id}`);
+      return data.id;
     } catch (err) {
-      console.log(err);
+      return thunkAPI.rejectWithValue(err);
     }
   }
 );
@@ -37,85 +35,227 @@ export const deleteTask = createAsyncThunk(
 export const inputTask = createAsyncThunk(
   "tasks/inputTask",
   async (data: { task: string }, thunkAPI) => {
-    console.log(data)
-    const task = data.task;
-
     try {
       const res = await axios.post(`${baseUrl}/.netlify/functions/api`, {
-        name: task,
+        name: data.task,
       });
-      return res.data;
+      return data.task;
     } catch (err) {
-      console.log(err);
+      return thunkAPI.rejectWithValue(err);
     }
   }
 );
+
 export const editTask = createAsyncThunk(
   "tasks/editTask",
-  async (data: {task: string, id:number}, thunkAPI) => {
-    const  task  = data.task
-    const id = data.id;
+  async (data: { task: string, id: number }, thunkAPI) => {
     try {
-      const res = await axios.put(`${baseUrl}/.netlify/functions/api/${id}`, {
-        name: task,
+      const res = await axios.put(`${baseUrl}/.netlify/functions/api/${data.id}`, {
+        name: data.task,
       });
-      return res.data;
+      return { task: data.task, id: data.id };
     } catch (err) {
-      console.log(err);
+      return thunkAPI.rejectWithValue(err);
     }
   }
 );
-
-
 
 export const taskSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {},
-    
-
-  
   extraReducers: (builder) => {
     builder
-    .addCase(getTasks.pending, (state) => {
-      state.tasks = [];
-    })
-    builder.addCase(getTasks.fulfilled, (state, payload) => {
-      console.log(payload.payload);
-      state.tasks =(payload.payload)
-    })
-    builder.addCase(getTasks.rejected, (state) => {
-      state.tasks = [];
-    })
-
-    builder.addCase(deleteTask.pending, (state) => {})
-    builder.addCase(deleteTask.fulfilled,(state, payload) => {
-      console.log(payload);
-      state.tasks = state.tasks.filter((task:any) => task.todo_id !== payload);
-    })
-    builder.addCase(deleteTask.rejected, (state) => {})
-
-    builder.addCase(inputTask.pending, (state) => {
-      state.tasks = [];
-    })
-    builder.addCase(inputTask.fulfilled, (state,payload)  => {
-      console.log(payload);
-      state.tasks =(payload.payload)
-    })
-    builder.addCase(inputTask.rejected,(state) => {
-      state.tasks = []
-    })
-    builder.addCase(editTask.pending, (state) => {
-      state.tasks = [];
-    })
-    builder.addCase(editTask.fulfilled,(state, payload) => {
-      console.log(payload);
-      state.tasks =(payload.payload)
-    })
-    builder.addCase(editTask.rejected, (state) => {
-      state.tasks = [];
-    })
+      .addCase(getTasks.pending, (state) => {
+        state.tasks = [];
+      })
+      builder.addCase(getTasks.fulfilled, (state, action) => {
+        state.tasks = action.payload;
+      })
+      builder.addCase(getTasks.rejected, (state) => {
+        state.tasks = [];
+      })
+      builder.addCase(deleteTask.pending, (state) => { })
+      builder.addCase(deleteTask.fulfilled, (state, action) => {
+        state.tasks = state.tasks.filter((task: any) => task !== action.payload);
+      })
+      builder.addCase(deleteTask.rejected, (state) => { })
+      builder.addCase(inputTask.pending, (state) => {
+        state.tasks = [];
+      })
+      builder.addCase(inputTask.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.tasks.push(action.payload);
+        }
+      })
+      builder.addCase(inputTask.rejected, (state) => {
+        state.tasks = [];
+      })
+      builder.addCase(editTask.pending, (state) => {
+        state.tasks = [];
+      })
+      builder.addCase(editTask.fulfilled, (state, action) => {
+        if (action.payload) {
+          const { task, id } = action.payload;
+          const index = state.tasks.findIndex((taskItem: any) => taskItem.id === id);
+          if (index !== -1) {
+            state.tasks[index] = task;
+          }
+        }
+      })
+      builder.addCase(editTask.rejected, (state) => {
+        state.tasks = [];
+      })
   }
 });
 
 export default taskSlice.reducer;
+
+
+
+
+
+
+
+
+
+// ***********************************************************************//
+
+
+
+
+
+
+
+
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import  axios  from "axios";
+
+// type  taskType = {
+//   tasks: string [],
+// }
+
+// const initialState: taskType = { 
+//   tasks: [],
+// }
+
+// const baseUrl = "https://project-4-server.netlify.app"
+
+// export const getTasks = createAsyncThunk("tasks/getTasks", async (thunkAPI) => {
+//   try {
+//     const res = await axios.get(`${baseUrl}/.netlify/functions/api`);
+//     return res.data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// export const deleteTask = createAsyncThunk(
+//   "tasks/deleteTask",
+//   async (data:{id:number}, thunkAPI) => {
+    
+//     const id = data.id
+//     try {
+//       await axios.delete(`${baseUrl}/.netlify/functions/api/${id}`);
+//       return id;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+// );
+
+// export const inputTask = createAsyncThunk(
+//   "tasks/inputTask",
+//   async (data: { task: string }, thunkAPI) => {
+//     console.log(data)
+//     const task = data.task;
+
+//     try {
+//       const res = await axios.post(`${baseUrl}/.netlify/functions/api`, {
+//         name: task,
+//       });
+//       return task;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+// );
+// export const editTask = createAsyncThunk(
+//   "tasks/editTask",
+//   async (data: {task: string, id:number}, thunkAPI) => {
+//     const  task  = data.task
+//     const id = data.id;
+//     try {
+//       const res = await axios.put(`${baseUrl}/.netlify/functions/api/${id}`, {
+//         name: task,
+//       });
+//       return { task, id };
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+// );
+
+
+
+// export const taskSlice = createSlice({
+//   name: "tasks",
+//   initialState,
+//   reducers: {},
+    
+
+//   extraReducers: (builder) => {
+//     builder
+//     .addCase(getTasks.pending, (state) => {
+//       state.tasks = [];
+//     })
+//     builder.addCase(getTasks.fulfilled, (state, payload) => {
+//       console.log(payload.payload);
+//       state.tasks =(payload.payload)
+//     })
+//     builder.addCase(getTasks.rejected, (state) => {
+//       state.tasks = [];
+//     })
+
+//     builder.addCase(deleteTask.pending, (state) => {})
+//     builder.addCase(deleteTask.fulfilled,(state, action) => {
+//       console.log(action.payload);
+//       state.tasks = state.tasks.filter((task:any) => task !== action.payload);
+
+//     })
+//     builder.addCase(deleteTask.rejected, (state) => {})
+
+//     builder.addCase(inputTask.pending, (state) => {
+//       state.tasks = [];
+//     })
+//     builder.addCase(inputTask.fulfilled, (state, action)  =>  {
+//       console.log(action.payload);
+//         if (action.payload) {
+//           state.tasks.push(action.payload);
+//         }
+//       });
+
+//       builder.addCase(inputTask.rejected, (state) => {
+//         state.tasks = [];
+//       });
+
+//       builder.addCase(editTask.pending, (state) => {
+//         state.tasks = [];
+//       });
+//       builder.addCase(editTask.fulfilled,(state, action) => {
+//         if (action.payload) {
+//           const { task, id } = action.payload;
+//           const index = state.tasks.findIndex((taskItem: any) => taskItem.id === id);
+//           if (index !== -1) {
+//             state.tasks[index] = task;
+//              }
+//             }
+//           });
+
+//     builder.addCase(editTask.rejected, (state) => {
+//       state.tasks = [];
+//     })
+//   }
+// });
+
+// export default taskSlice.reducer;
